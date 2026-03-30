c34868cda4a7970fa62e6dbc7f93151baf5a03ea
Benloko <benoit6297@gmail.com>
2026-03-30 04:16:04 +0100

    COnfiguration BD

--- config/config.local.php
+++ config/config.local.php
@@ -2,7 +2,7 @@
 // Configuration locale (pour ton PC)
 return [
     'DB_HOST' => '127.0.0.1',
-    'DB_USER' => 'gpower_user',
-    'DB_PASS' => 'gpower_pass_2025',
-    'DB_NAME' => 'gpower_db'
+    'DB_USER' => 'u268450082_ben',
+    'DB_PASS' => 'Gpower@store62',
+    'DB_NAME' => 'u268450082_gpower'
 ];
